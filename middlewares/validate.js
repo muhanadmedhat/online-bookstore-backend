cd7b20243319dea68b7f89892f952e4575c5cac0
Haneenelasawy <Haneenelasawy335@gmail.com>
2026-02-20 00:04:12 +0200

    add validation middlewares

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,3 +1,20 @@
+module.exports = (schema) => (req, res, next) => {
+  const { error, value } = schema.validate(req.body, {
+    abortEarly: false,
+    stripUnknown: true,
+  });
+
+  if (error) {
+    return res.status(400).json({
+      message: "Validation error",
+      details: error.details.map((d) => d.message),
+    });
+  }
+
+  req.body = value;
+  next();
+};
+
 // Suggested approach
 
 // validations/user.js
