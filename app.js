2aa12767f6d06fa301bec3a3c5b2c740842b1b5e
asaadmansour <as89aadmansour@gmail.com>
2026-02-22 15:48:30 +0200

    fixed the linter

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger.js');
 const CustomError = require('./helpers/CustomError');
