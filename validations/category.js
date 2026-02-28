8fa8942bd7402cda641b9c0b8a153acaaabba9bc
Haneenelasawy <Haneenelasawy335@gmail.com>
2026-02-28 17:49:35 +0200

    fix category valitation

--- validations/category.js
+++ validations/category.js
@@ -1,11 +1,13 @@
 const Joi = require('joi');
 
 const createCategorySchema = Joi.object({
-  name: Joi.string().trim().min(2).max(50).required()
+  name: Joi.string().trim().min(2).max(50).required(),
+  description: Joi.string().allow('').optional()
 });
 
 const updateCategorySchema = Joi.object({
-  name: Joi.string().trim().min(2).max(50)
+  name: Joi.string().trim().min(2).max(50),
+  description: Joi.string().allow('').optional()
 }).min(1);
 
 module.exports = {
