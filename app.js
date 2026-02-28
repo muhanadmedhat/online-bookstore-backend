08683e48c89e826faf0072d5b54b3d79866255d7
asaadmansour <as89aadmansour@gmail.com>
2026-02-28 23:14:21 +0200

    added ratelimiter - logger - helmet

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 const cors = require('cors');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger.js');
 const CustomError = require('./helpers/CustomError');
+const logger = require('./helpers/logger.js');
 const router = require('./routes');
 
 const app = express();
-app.use(cors());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ['\'self\''],
+        imgSrc: ['\'self\'', 'https://res.cloudinary.com'],
+        scriptSrc: ['\'self\''],
+        styleSrc: ['\'self\'']
+      }
+    }
+  })
+);
 
-app.use('/health', (req, res) => {
+app.set('trust proxy', 1);
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message) => logger.http(message.trim())
+    }
+  })
+);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 300
+});
+
+app.use('/', limiter, router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/health', (req, res) => {
   res.json({message: 'API is running'});
 });
 
-app.use(router);
-
 app.use((error, req, res, next) => {
-  console.error('ERROR:', `${error}`);
+  logger.error({
+    message: error.message,
+    stack: error.stack
+  });
   if (error instanceof CustomError) {
     res.status(error.statusCode).json({error: error.message});
   } else {
