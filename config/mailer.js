4ab3010a8bff67f2d2933ca4819d144de85fc29e
asaadmansour <as89aadmansour@gmail.com>
2026-03-01 04:30:19 +0200

    fix(mailer): force family: 4 to prevent ipv6 ENETUNREACH in nodemailer

--- config/mailer.js
+++ config/mailer.js
@@ -2,13 +2,17 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  connectionTimeout: 10000, // 10 seconds
-  greetingTimeout: 5000, // 5 seconds
-  socketTimeout: 10000 // 10 seconds
+  family: 4, 
+  connectionTimeout: 10000, 
+  greetingTimeout: 5000,
+  socketTimeout: 10000 
 });
 
 module.exports = transporter;
