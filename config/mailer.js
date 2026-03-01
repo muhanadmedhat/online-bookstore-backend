a222d1ca0d73fbb837d4962f2de8d46311e58f2e
asaadmansour <as89aadmansour@gmail.com>
2026-03-01 05:00:33 +0200

    nodemailer config

--- config/mailer.js
+++ config/mailer.js
@@ -3,15 +3,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
   family: 4, 
   connectionTimeout: 10000, 
-  greetingTimeout: 5000,
+  greetingTimeout: 5000, 
   socketTimeout: 10000 
 });
 
--- server.js
+++ server.js
@@ -1,22 +1,5 @@
 require('dotenv').config();
 
-// ✅ FORCE IPv4 FIRST (Fix Railway + Gmail ENETUNREACH)
-// Monkey-patch dns.lookup to absolutely forbid IPv6 resolution
-const dns = require('node:dns');
-const originalLookup = dns.lookup;
-dns.lookup = function(hostname, options, callback) {
-  if (typeof options === 'function') {
-    callback = options;
-    options = { family: 4 };
-  } else if (typeof options === 'object') {
-    options = { ...options, family: 4 };
-  } else if (typeof options === 'number') {
-    options = { family: 4 };
-  }
-  return originalLookup(hostname, options, callback);
-};
-dns.setDefaultResultOrder('ipv4first');
-
 const app = require('./app');
 const connectDB = require('./config/db');
 
